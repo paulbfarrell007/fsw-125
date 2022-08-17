e584733a17de616a9580b84d0b2509f20426ec0d
Paul Farrell <87060717+paulbfarrell007@users.noreply.github.com>
2022-08-16 22:09:32 -0400

    Add files via upload

---
+++ week1/math/math.js
@@ -0,0 +1,9 @@
+const difference = (a, b) => {
+    return(a - b)}
+const dividend = (a, b) => {
+    return(a / b)}    
+const sum = (a, b) => {
+    return(a + b)}    
+const product = (a, b) => {
+    return(a * b)}
+module.exports = {difference, dividend, sum, product}
