5c6f745cfa12019705da078f5191848d92d6b875
Paul Farrell <87060717+paulbfarrell007@users.noreply.github.com>
2022-08-16 22:08:23 -0400

    Delete math directory

--- math/app.js
+++
@@ -1,5 +0,0 @@
-const mathObj = require('./math')
-console.log(mathObj.difference(1,2))
-console.log(mathObj.dividend(1,2))
-console.log(mathObj.sum(1,2))
-console.log(mathObj.product(1,2))
--- math/math.js
+++
@@ -1,9 +0,0 @@
-const difference = (a, b) => {
-    return(a - b)}
-const dividend = (a, b) => {
-    return(a / b)}    
-const sum = (a, b) => {
-    return(a + b)}    
-const product = (a, b) => {
-    return(a * b)}
-module.exports = {difference, dividend, sum, product}
